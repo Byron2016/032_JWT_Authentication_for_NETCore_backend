5fcfc953b974bfcdf7b5ecee2601e5ff79025ca7
Byron 2016 <bgva2005@yahoo.com>
2021-12-28 16:29:13 -0500

    fix: add token generator

---
+++ JwtAutDemo/Areas/jwt/Controllers/TokenController.cs
@@ -0,0 +1,26 @@
+﻿using JwtAutDemo.Areas.jwt.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace JwtAutDemo.Areas.jwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public TokenController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public string GetRandomToken()
+        {
+            var jwt = new JwtService(_config); //using JwtAutDemo.Areas.jwt.Services;
+            var token = jwt.GenerateSecurityToken("fake@email.com");
+            return token;
+        }
+    }
+}
